3b5abd1505a4e9ef5a811b06d735a75b5c4c78d6
Eddy Leon <xltunited@gmail.com>
2016-11-10 20:48:52 -0500

    debugging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require('method-override');
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
